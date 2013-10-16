ee80859ab0a4ecff016e1123acf47126e7cc0e0e
Ella_Astashonok <Ella_Astashonok@epam.com>
2013-10-16 19:34:15 +0300

    Initial

---
+++ TestKalmanFilter/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TestKalmanFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.Name = "ChartArea1";
+            chartArea2.Name = "ChartArea2";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea2";
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(749, 463);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 463);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 25);
+            this.panel1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(13, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(62, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Show 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(81, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Show 2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 488);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ TestKalmanFilter/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestKalmanFilter
+{
+    using System.Windows.Forms.DataVisualization.Charting;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.TestFilter();
+        }
+
+        private double sigmaPsi = 5d;
+        private double sigmaEta = 50d;
+
+        public void TestFilter()
+        {
+            
+            var a = 0.1;
+            var N = 100;
+            var values = new double[N];
+            var modelValues = new double[N];
+            var realValues = new double[N];
+
+            var val = 0d;
+            var rnd = new Random();
+
+            chart1.Series.Clear();
+
+            for (int i = 0; i < N; i++)
+            {
+                modelValues[i] = a * (i * i);
+                var rVal = modelValues[i] + (rnd.NextDouble() * 2 * sigmaPsi) - sigmaPsi;
+                realValues[i] = rVal;
+                val = rVal + (rnd.NextDouble() * 2 * sigmaEta) - sigmaEta;
+                values[i] = val;
+            }
+            var res = this.Filter(values, modelValues);
+
+            this.AddChart("Real Values", realValues, Color.CadetBlue, chart1.ChartAreas[0]);
+            this.AddChart("Raw Values", values, Color.Brown, chart1.ChartAreas[0]);
+            this.AddChart("Processed Values", res, Color.DarkGreen, chart1.ChartAreas[0]);
+
+            this.AddChart("e Opt", eOpt, Color.DarkMagenta, chart1.ChartAreas[1]);
+            this.AddChart("K", K, Color.DarkGoldenrod, chart1.ChartAreas[1]);
+
+        }
+
+        double[] eOpt;
+        double[] xOpt;
+        double[] K;
+
+        public double[] Filter(double[] values, double[] modelValues)
+        {
+            eOpt = new double[values.Length];
+            xOpt = new double[values.Length];
+            K = new double[values.Length];
+
+
+            var s = 0d; //СКО 
+            for (int i = 1; i < values.Length; i++)
+            {
+                var val = values[i];
+                var mVal = modelValues[i];
+                var err = Math.Abs(val - mVal);
+                s = (s * (i - 1) + err) / i;
+            }
+            var sigmaEtaCalc = s;
+            var sigmaPsiCalc = 1d;
+
+            xOpt[0] = values[0];
+            eOpt[0] = sigmaEtaCalc;
+            for (int t = 0; t < values.Length - 1; t++)
+            {
+                //eOpt[t + 1] =
+                //    Math.Sqrt( 
+                //    sigmaEtaCalc.pow(2) * (eOpt[t].pow(2) + sigmaPsiCalc.pow(2)) / 
+                //    (sigmaEtaCalc.pow(2) + eOpt[t].pow(2) + sigmaPsiCalc.pow(2))
+                //    );
+                eOpt[t + 1] = Math.Sqrt( sigmaEtaCalc.pow(2) * eOpt[t].pow(2) / (sigmaEtaCalc.pow(2) + eOpt[t].pow(2)) );
+                K[t + 1] = eOpt[t + 1].pow(2) / sigmaEtaCalc.pow(2);
+                xOpt[t + 1] = (modelValues[t + 1]) * (1 - K[t + 1]) + K[t + 1] * values[t + 1];
+            }
+            return xOpt;
+        }
+
+        public void AddChart(string name, double[] val, Color color, ChartArea area)
+        {
+            var ser = new Series(name) {ChartArea  = area.Name, ChartType = SeriesChartType.Line, Color = color };
+            for (int i = 0; i < val.Length; i++)
+            {
+                ser.Points.AddXY(i, val[i]);
+            }
+            chart1.Series.Add(ser);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].Visible = checkBox1.Checked;
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[1].Visible = checkBox2.Checked;
+        }
+
+        private void chart1_MouseClick(object sender, MouseEventArgs e)
+        {
+            HitTestResult result = chart1.HitTest(e.X, e.Y);
+            if (result != null && result.Object != null)
+            {
+                // When user hits the LegendItem
+                if (result.Object is LegendItem)
+                {
+                    // Legend item result
+                    LegendItem legendItem = (LegendItem)result.Object;
+                }
+            }
+        }
+    }
+
+    public static class powExtensions
+    {
+        public static double pow(this double val, double pow)
+        {
+            return Math.Pow(val, pow);
+        }
+    } 
+}
